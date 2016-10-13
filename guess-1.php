5135714bf0055916e575e747335406c28ecce5fc
o_revollat <revollat@gmail.com>
2016-10-13 13:49:04 +0000

    ok

---
+++ guess-1.php
@@ -0,0 +1,34 @@
+<html>
+<body>
+<?php
+session_start();
+
+// Initialize un nombre aléatoire entre 1 et 100 en session pour jouer
+if (!isset($_SESSION['nombre_a_trouver'])) {
+    $_SESSION['nombre_a_trouver'] = rand (1,100) ;
+}
+
+//echo "Nombre : " . $_SESSION['nombre_a_trouver'];
+
+if ( isset($_POST['proposition']) ) {
+    $proposition = (int) $_POST['proposition'];
+    $gagne = $proposition === $_SESSION['nombre_a_trouver'];
+    $message = $proposition < $_SESSION['nombre_a_trouver'] ? "Trop petit" : "Trop grand";
+}
+?>
+
+
+<?php if( isset($gagne) && !$gagne ): ?>
+    <font size="6">&#x1f63f; <?php echo $message; ?> ... Essayes encore ...</font>
+<?php elseif( isset($gagne) && $gagne ): ?>
+    <font size="6">&#x1f63a; Bazinga !!!</font>  
+<?php endif; ?>
+
+
+<form method="POST">
+    <input type="text" name="proposition" />
+    <input type="submit" value="Tenter ma chance !" />
+</form>
+
+</body>
+</html>
